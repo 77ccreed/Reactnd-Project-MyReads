437825204998814c63d0bfef834d00cf77a28a0e
Kiur Jullis <kiur.jullis@gmail.com>
2018-07-24 12:42:16 +0300

    import books api. componentdidmount

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react'
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI'
 import './css/App.css'
 import SearchPage from './components/SearchPage'
 import MainPage from './components/MainPage'
 
 class BooksApp extends React.Component {
 
+state= {
+  books:[];
+}
+
+componentDidMount(){
+  BooksAPI.getAll().then((books) =>{
+    this.setState({books})
+  })
+}
 
   render() {
     return ( <
