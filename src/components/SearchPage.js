0f022ae442552f34334afd9d1acf606d516321f2
Kiur Jullis <kiur.jullis@gmail.com>
2018-07-24 11:46:54 +0300

    import book

--- src/components/SearchPage.js
+++ src/components/SearchPage.js
@@ -1,21 +1,25 @@
 import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
+/*import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
-
+*/
+import Book from './Book'
 class SearchPage extends Component{
   render() {
     return (
       <div className="search-books">
+
         <div className="search-books-bar">
           <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
+
           <div className="search-books-input-wrapper">
             {
-              
+
             }
             <input type="text" placeholder="Search by title or author" />
 
           </div>
         </div>
+
         <div className="search-books-results">
           <ol className="books-grid"></ol>
         </div>
@@ -24,11 +28,6 @@ class SearchPage extends Component{
   }
 }
 
-BooksSearch.propTypes = {
-  result: PropTypes.array.isRequired,
-  currentQuery: PropTypes.string.isRequired,
-  onQueryInput: PropTypes.func.isRequired,
-  onChangeShelf: PropTypes.func.isRequired
-}
+
 
 export default SearchPage;
