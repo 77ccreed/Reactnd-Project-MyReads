1e9f4bb9299a64ede6018bf2b2cffc5b976aedc4
Kiur Jullis <kiur.jullis@gmail.com>
2018-07-24 18:42:20 +0300

    ..

--- src/components/Book.js
+++ src/components/Book.js
@@ -8,8 +8,8 @@ class Book extends Component{
         <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${this.props.book.imageLinks.thumbnail}")` }}></div>
         <div className="book-shelf-changer">
           <select
-          onChange={() => this.props.moveShelf(
-            this.props.book
+          onChange={(e) => this.props.moveShelf(
+            this.props.book, e.target.value
           )}
           >
             <option value="move" disabled>Move to...</option>
