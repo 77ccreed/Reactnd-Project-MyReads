e3e575618a0c68618adbc59ce51b130b2a3d743f
Kiur Jullis <kiur.jullis@gmail.com>
2018-07-25 08:05:42 +0300

    event target value

--- src/components/Book.js
+++ src/components/Book.js
@@ -8,8 +8,8 @@ class Book extends Component{
         <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${this.props.book.imageLinks.thumbnail}")` }}></div>
         <div className="book-shelf-changer">
           <select
-          onChange={(e) => this.props.moveShelf(
-            this.props.book, e.target.value
+          onChange={(event) => this.props.moveShelf(
+            this.props.book, event.target.value
           )}
           >
             <option value="move" disabled>Move to...</option>
