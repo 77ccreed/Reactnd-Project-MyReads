221bb27c4c49d691e254fc7fea8fa707ec0157bc
Kiur Jullis <kiur.jullis@gmail.com>
2018-07-26 11:39:20 +0300

    import Link

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import Book from './Book'
+import { Link } from 'react-router-dom'
 /*import PropTypes from "prop-types"*/
 
 class MainPage extends Component{
