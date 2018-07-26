f8d73f63b362c31ba09f2882fe031656afd9f899
Kiur Jullis <kiur.jullis@gmail.com>
2018-07-26 08:34:50 +0300

    ..

--- src/App.js
+++ src/App.js
@@ -30,16 +30,17 @@ class BooksApp extends React.Component {
 
   render() {
     return ( <
-      div className = "app" >
-      <
-      MainPage books = {
-        this.state.books
-      }
-      moveShelf = {
-        this.moveShelf
-      }
+      div className="app" >{/*<
+        MainPage books={
+          this.state.books
+        }
+        moveShelf={
+          this.moveShelf
+        }
       />
-
+      */}
+  
+<SearchPage />
       <
       /div>
     )
