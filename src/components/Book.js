1e6b7f4f3ad85db4954684a12c8bb01294ee7ece
Kiur Jullis <kiur.jullis@gmail.com>
2018-07-27 12:05:39 +0300

    onChange

--- src/components/Book.js
+++ src/components/Book.js
@@ -11,7 +11,7 @@ class Book extends Component{
         <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${displayedThumbnail}")` }}></div>
         <div className="book-shelf-changer">
           <select
-          onChange={(event) => this.props.currentShelf(
+          onChange={(event) => this.props.moveShelf(
             this.props.book, event.target.value
           )}
           value={this.props.currentShelf}
