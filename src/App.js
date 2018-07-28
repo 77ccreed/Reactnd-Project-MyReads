38adad641ec66961aed7d9039218f6f497e7731b
Kiur Jullis <kiur.jullis@gmail.com>
2018-07-28 08:12:00 +0300

    update moveShelf function

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react'
-import { Route } from 'react-router-dom'
+import {
+  Route
+} from 'react-router-dom'
 import * as BooksAPI from './data/BooksAPI'
 import './css/App.css'
 import SearchPage from './components/SearchPage'
@@ -20,38 +22,51 @@ class BooksApp extends React.Component {
   }
 
   moveShelf = (book, shelf) => {
-    BooksAPI.update(book, shelf);
-    
-    BooksAPI.getAll().then((books) => {
-      this.setState({
-        books: books
+    BooksAPI.update(book, shelf)
+      .then(() => {
+        BooksAPI.getAll().then(books => {
+          this.setState({
+            books
+          })
+        })
+
       })
-    })
+
   }
 
   render() {
     return ( <
       div className = "app" >
 
-      <Route
-        exact path="/"
-        render={() => (
-          <MainPage
-            books={this.state.books}
-            moveShelf={this.moveShelf}
+      <
+      Route exact path = "/"
+      render = {
+        () => ( <
+          MainPage books = {
+            this.state.books
+          }
+          moveShelf = {
+            this.moveShelf
+          }
           />
-        )}
+        )
+      }
       />
 
 
-      <Route
-         path="/search"
-        render={() => (
-          <SearchPage
-            moveShelf={this.moveShelf}
-            books={this.state.books}
+      <
+      Route path = "/search"
+      render = {
+        () => ( <
+          SearchPage moveShelf = {
+            this.moveShelf
+          }
+          books = {
+            this.state.books
+          }
           />
-        )}
+        )
+      }
       />
 
       <
